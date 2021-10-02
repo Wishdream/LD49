cb2ba1e2553960839260707188c81fc3733edff8
John Jerome "Wishdream" Romero <influxWishdream@gmail.com>
2021-10-02 16:01:34 +0800

    Added initial player files added

---
+++ Characters/Player/Player.gd
@@ -0,0 +1,19 @@
+extends KinematicBody2D
+
+var hpmax
+var hp
+
+var move_speed
+var jump_speed
+var dash_speed
+
+var vel
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
